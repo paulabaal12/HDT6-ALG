782076f2625ec0dd3b96393283c50c50ee41f48e
paulabaal12 <106341373+paulabaal12@users.noreply.github.com>
2023-03-06 22:09:21 -0600

    AJUSTES

--- Funciones/HashMap.java
+++ Funciones/HashMap.java
@@ -1,14 +1,38 @@
 package Funciones;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+
+import java.util.Scanner;
+
 
 public class HashMap implements Imap {
+    private static final String[][] Listdeproductos = null;
+    private String name;
 
+    public HashMap(String name) {
+        this.name = name;
+    }
+    /**
+     *
+     */
+    HashMap<String, Integer> productos = new HashMap<String, Integer>();
+
+   
     @Override
     public String agregarProducto(String Categoria, String Producto) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'agregarProducto'");
-    }
+        /*for (String[]producto : Listdeproductos){
+            productos.put(newProductos.getName());*/
+        }
+        HashMap<String, String> map  = new HashMap<String, String>();
+
+    // key-value pairs
+        System.out.println("Bienvenido ");
+        System.out.println("Ingrese el producto que desea agregar");
+        map = sc.nextLine();
+        map.put("-", " ");
+        map.put("-", "");
+        
 
     @Override
     public String Mostrarcategorias(String producto) {
--- Funciones/archivo.java
+++ Funciones/archivo.java
@@ -2,11 +2,15 @@ package Funciones;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 public class archivo {
     public static void main(String[] args) {
         String fileName = "ListadoProducto.txt";
         String line = null;
+        Scanner scanner = new Scanner(fileName);
 
         try {
             FileReader fileReader = new FileReader(fileName);
@@ -15,6 +19,7 @@ public class archivo {
 
             while ((line = bufferedReader.readLine()) != null) {
                 System.out.println(line);
+                String[] columnas = line.split("|"); // Separador de columna
             }
 
             bufferedReader.close();
